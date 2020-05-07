b1915f9653c6681f53dba4a1788859c6f99b4655
alexantg <64732630+alexantg@users.noreply.github.com>
2020-05-07 23:19:53 +0200

    Changes

--- Eksamensoppgaver/task7_2019.ino/task7_2019.ino.ino
+++ Eksamensoppgaver/task7_2019.ino/task7_2019.ino.ino
@@ -1,11 +1,39 @@
 
+const int speakerPin= 8;
+const int photoLedPin=A0;
+const int ledPin= 4;
+
+int sensorLow=1023;
+int sensorHigh=-1;
+
+int photoRead;
 
 void setup() {
-  
+  photoRead= analogRead(photoLedPin);
+  unsigned long time= millis();
 
+  pinMode(speakerPin, OUTPUT);
+  pinMode(ledPin, OUTPUT)
+
+  //loop runs for 5 seconds//
+  while(time<5000){
+    if(photoRead>sensorHigh){
+        sensorHigh= photoRead;  
+    }
+    if(photoRead<sensorLow){
+      sensorLow= photoRead;
+    }
+  }
+  digitalWrite(ledPin, HIGH);
+  
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  photoRead= analogRead(photoLedPin);
 
+  delay(20)
+}
+
+int skaler(){
+  
 }
