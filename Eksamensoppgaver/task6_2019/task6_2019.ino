93ec71546d4b499f4b520bb9edd0f64d96f1ba37
alexantg <64732630+alexantg@users.noreply.github.com>
2020-05-07 22:49:21 +0200

    Added file

--- Eksamensoppgaver/task6_2019/task6_2019.ino
+++ Eksamensoppgaver/task6_2019/task6_2019.ino
@@ -1,10 +1,39 @@
-void setup() {
+#define TEST 1
+void setup()
+{
+Serial.begin(9600);
+Serial.print("Arduino Test har startet: ");
+Serial.println(TEST);
+}
+void loop(){
+  
+  int resultat = 0, antall = 4, bokser = 8;
+  float verdi = 0.0;
+  char tekst1[30] = "Sommer";
+  char tekst2[30] = "2019";
+  
+  resultat = antall/bokser;
+  Serial.println("resultat = " + (String) resultat);
+  resultat = antall%bokser;
+  Serial.println("resultat = " + (String) resultat);
+  verdi = antall/(float) bokser;
+  Serial.println("verdi = " + (String) verdi);
+  strcat(tekst1," ");
+  strcat(tekst1,tekst2);
+  Serial.println(tekst1);
+  Serial.println(strlen(tekst1));
+  funksjonTest(antall, &bokser);
+  Serial.println("bokser = " + (String) bokser);
 
-  int i =0;
-
+  while(1)
+  { }
 }
 
-void loop() {
-
-
+void funksjonTest(int ant, int *pkr){
+  
+  int res = 0;
+  res = ant+(*pkr);
+  Serial.println("res = " + (String) res);
+  *pkr = 42;
+  
 }
