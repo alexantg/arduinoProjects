23dcb4a9edb4ccc0482554cc9b56b6d8e8e91d0b
alexantg <64732630+alexantg@users.noreply.github.com>
2020-05-08 11:55:26 +0200

    Finished task 7

--- Eksamensoppgaver/task7_2019/task7_2019.ino
+++ Eksamensoppgaver/task7_2019/task7_2019.ino
@@ -6,6 +6,9 @@ const int ledPin= 4;
 int sensorLow=1023;
 int sensorHigh=-1;
 
+int lowFreq=100;
+int highFreq= 4000;
+
 int photoRead;
 
 void setup() {
@@ -29,11 +32,20 @@ void setup() {
 }
 
 void loop() {
+  int freq=0;
   photoRead= analogRead(photoLedPin);
 
+  freq= skaler(skaler(photoRead))
+
+  tone(speakerPin, freq, 20);
+  
   delay(20)
 }
 
-int skaler(){
-  
+int skaler(int sensorValue){
+  int frequency=0;
+
+  frequency=(float (sensorValue-sensorLow)/(sensorHigh-sensorLow))(highFreq-lowFreq) + lowFreq;
+
+  return frequency;
 }
